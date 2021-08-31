4488028961280cacaa259b2e1c3411e199eb2f27
Jason Ventura <jjventura@shaw.ca>
2021-08-31 19:59:30 +0000

    Add NLU config

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -12,8 +12,8 @@ app.use(cors_app());
 /*Uncomment the following lines to loan the environment 
 variables that you set up in the .env file*/
 
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 // const api_key = process.env.API_KEY;
 // const api_url = process.env.API_URL;
@@ -22,6 +22,20 @@ function getNLUInstance() {
     /*Type the code to create the NLU instance and return it.
     You can refer to the image in the instructions document
     to do the same.*/
+    const api_key = process.env.API_KEY;
+    const api_url = process.env.API_URL;
+
+    const naturalLanguageUnderstanding = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2021-08-31',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
 }
 
 
